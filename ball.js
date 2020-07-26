85e12601199893bcd16fa48c385a12c5239529be
RithvikJandhyala <67884428+RithvikJandhyala@users.noreply.github.com>
2020-07-26 14:30:14 -0700

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,28 @@
+class Ball {
+    constructor(x,y) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2,
+      }
+      this.body = Bodies.circle(x, y, 50, options);
+      this.diamter = 50;
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("pink");
+      stroke("brown");
+      strokeWeight(4);
+      circle(0,0, this.diamter);
+      pop();
+    }
+  
+  };
